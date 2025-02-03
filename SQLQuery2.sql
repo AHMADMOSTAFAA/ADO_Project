39d386d89b844f5855f226bc8015eedbdf4f940f
Ahmad Mostafa <ahmadmostafaa02@gmail.com>
2025-02-03 06:42:56 +0200

    removed unessecary files

--- SQLQuery2.sql
+++
@@ -1,4 +0,0 @@
-﻿select p.productid ,p.productname,p.price from products p inner join  favorites f on f.PId=P.ProductId where uid=9
-delete from favorites where pid=1 and uid=9
-ALTER TABLE Products ALTER COLUMN Price DECIMAL(10,2);
-Select * from Users where username="n" and password='{password'
